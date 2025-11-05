18e8125126dfdbae01fe873b6070a2dc954535e7
정문수 <munsujeong10@gmail.com>
2025-11-05 21:04:09 +0900

    별거아닌거 수정

--- Assets/Scripts/MUN/Player_interaction.cs
+++ Assets/Scripts/MUN/Player_interaction.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 
 public class Player_Interaction : MonoBehaviour
 {
-    public float radius;
+    public float radius; //Inspector 창에서 범위 설정
     public LayerMask interactableLayer;
-    private void Update()
+    private void Update() //상호작용 가능한 오브젝트, NPC를 계속 찾을 수 있도록
     {
-        var interactableObj = Physics2D.OverlapCircleAll(transform.position, radius, interactableLayer);
+        var interactableObj = Physics2D.OverlapCircleAll(transform.position, radius, interactableLayer);  // 상호작용 범위를 만들 원
         if (interactableObj.Length > 0)
         {
             foreach (var item in interactableObj)
             {
-                item.GetComponent<Interaction>().isPlayerInRange = true;
+                item.GetComponent<Interaction>().isPlayerInRange = true; // 상호작용 가능한 오브젝트, NPC가 들어왔는가?
             }
         }
     }
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.red; //빨간원
         Gizmos.DrawWireSphere(transform.position, radius);
     }   
 }
