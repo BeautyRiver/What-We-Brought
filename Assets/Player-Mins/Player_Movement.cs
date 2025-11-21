04512a6f41890897a17360ada23f8a35e88275c7
MinKang Kim <kmg05027@gmail.com>
2025-11-21 21:45:33 +0900

    fix: Player model, controller 분리

--- Assets/Player-Mins/Player_Movement.cs
+++ Assets/Player-Mins/Player_Movement.cs
@@ -11,7 +11,7 @@ public class Player_Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();        
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
         cameraTransform = GetComponent<Transform>();
     }
 
