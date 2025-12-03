b71474aa62286e4dd03c045c47a0cd8ab7e716f8
Gittcityboi <pushedwest0221@gmail.com>
2025-12-03 21:47:59 +0900

    12/03
    일단 브랜치 쓸모 없는거 업로드

--- Assets/Minseo/PlayerRotate.cs
+++ Assets/Minseo/PlayerRotate.cs
@@ -17,8 +17,7 @@ public class PlayerRotate : MonoBehaviour
             // 현재 크기 값을 가져옵니다.
             Vector3 currentScale = transform.localScale;
 
-            // horizontalInput이 양수(오른쪽)면 1, 음수(왼쪽)면 -1
-            // Mathf.Abs를 쓰는 이유: 혹시 모를 크기 오류 방지 (항상 양수에서 시작)
+            // horizontalInput이 양수(오른쪽)면 -1, 음수(왼쪽)면 1
             currentScale.x = Mathf.Abs(currentScale.x) * (horizontalInput > 0 ? -1 : 1);
 
             // 변경된 크기 적용
