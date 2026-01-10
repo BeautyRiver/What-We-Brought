a7ccd8312b41ae473a0578b9173f3eca596a5f9d
Gittcityboi <pushedwest0221@gmail.com>
2026-01-10 19:36:43 +0900

    1/10
    시킨 거 하는 중임다 월욜 전에 마칠 예정

--- Assets/Minseo/Scripts/Slot.cs
+++ Assets/Minseo/Scripts/Slot.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] Image image;
+    [SerializeField] Outline outline;   // Slot Item에 있는 Outline
+
+    public void SetHighlight(bool on)
+    {
+        if (outline != null)
+            outline.enabled = on;
+    }
 
     private Item _item;
     public Item item
@@ -26,6 +34,35 @@ public class Slot : MonoBehaviour
             }
         }
     }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_item == null)
+            return;
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            var eq = EquipmentManager.Instance;
+            if (eq == null)
+            {
+                Debug.LogWarning("EquipmentManager가 씬에 없습니다.");
+                return;
+            }
+
+            // 이미 이 슬롯 아이템이 장착 중이면 해제
+            if (eq.equippedItem == _item)
+            {
+                eq.Unequip();
+                eq.SetEquippedSlot(null);      // 여기까지 OK
+            }
+            else
+            {
+                eq.Equip(_item);               // 장착 아이템 교체
+                eq.SetEquippedSlot(this);      // ★ 새 슬롯을 넘겨줘야 함
+            }
+        }
+    }
+
     void Start()
     {
         
